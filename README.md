fd42bf0688721bc9420a40b2958e240d6d9d4b4b
Alux <plutocoding@gmail.com>
2023-11-30 20:50:51 -0500

    Setting up file structure, planning

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # week10SVGwow
 Object-oriented Programming Challenge: SVG Logo Maker (node.js image generation)
+
+**Welcome!**
+In this assignment we are instructed to make a node.js command-line application that takes in user input to generate a logo and save it as an SVG file.
+As the app is initated, users will be instructed to a color, shape, and text to make their logo design.
+
+## How to Run the app
+You can run this application by going into the terminal command line of the main file, and typing: node index.js, which will biegin the prompts.
+
+Helpful resources:
+- https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Basic_Shapes
+- https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Texts
+- - svg text offers various properties similar to css! (font family, weight, color, etc)
+
---
+++ index.js
@@ -0,0 +1,31 @@
+// in command line, npm i (bring in inquierer)
+// may have to bring in jest for testing and efficiency
+
+
+// we will need to require inquirer 
+// we will need to rquire fs (already built in)
+// const fs = require('fs');
+
+// we should setup other js files for class constructors of shapes, then require those
+
+// we will prompt user with inquirer to answer questions based on how they want their logo (color, shape, text,)
+// and if we have time maybe text properties, like font family, weight, color, etc
+// make sure the user doesn't enter too many characters for the text, as it could cause issues with the logo (if statement)
+
+
+// write files
+// at the end of the index.js, we can call the function to prompt the questions
+
+
+// Why are we going to use constructors? (xpert learning assistance AI on constructors and classes)
+// Imagine you have a blueprint for creating cars. 
+// This blueprint is like a class, and each car you create from this blueprint is an object.
+// Now, when you create a new car, you want to set its initial properties, such as the make, model, and color. 
+// This is where the constructor comes in.
+// The constructor is like a special function that runs automatically when you create a new car object. 
+// It is responsible for initializing the object with the desired values.
+
+// tldr
+// We will use classes to define what a logo can be, constructors to create new logos with specific details, 
+// and objects to represent the actual logos we create. 
+// It helps us organize and manage our logos in a structured and organized way.
