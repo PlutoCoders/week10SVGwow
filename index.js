7599e8d07e06813515505efa6bb77f4494f78482
Alux <plutocoding@gmail.com>
2023-12-05 17:59:16 -0500

    destructuring, handle user answers and set up logo

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const inquirer = require("inquirer");
 // in command line, npm i (bring in inquierer)
 // may have to bring in jest for testing and efficiency
 // Open the in-editor Terminal in VS Code (ctrl-`) and type yarn jest-w (or npm run jest-w )
-
+// Note: when importing fs like this, you bring in the entire node.js fs module, which allows you to use all of its methods.
+// Since we are only using the fs write method, how can we import only the write method
+// The benefits of only bringing in exactly what you need: Keeps codebase clean, reduces overhead (additional/extra work for a program to perform), and makes it clear what actual functionality your code is utilizing.
 const fs = require('fs');
 
 // Bring in the different modular js files
@@ -14,6 +16,8 @@ const {Circle, Triangle, Square} = require("./lib/shapes");
 // we will prompt user with inquirer to answer questions based on how they want their logo (color, shape, text,)
 // and if we have time maybe text properties, like font family, weight, color, etc
 // make sure the user doesn't enter too many characters for the text, as it could cause issues with the logo (if statement)
+
+// We need to bring in f
 
 // Note: we are declaring a function here, but this function will have to later be called, otherwise the user won't get prompted
 function promptUser() {
@@ -50,7 +54,25 @@ function promptUser() {
       // then we will generate these logos using svg.render()
       // next we will have to write the file writefile() and choose where it is written to (like a specific folder such as examples)
     ])
-};
+    // Here we begin the chaining
+    .then(({ text, textColor, shapeType, shapeColor }) => {
+      let ShapeClass = Circle;
+      // The reason we are using the .toLowerCase() string method is so that the user input will return true even if the have capitals in the text that they input in the earlier prompt.
+      // Having this improves user experience, and contributes to error handling, as it prevents potential errors
+      if (shapeType.toLowerCase() === 'triangle') {
+        ShapeClass = Triangle;
+        // Here we are continuing to handle the different choices that the user can choose. In the future, how can you improve this by using something like a switch statement?
+      } else if (shapeType.toLowerCase() === 'square') {
+        ShapeClass = Square;
+      }
+      // Setting the shape color to the one chosen by the user
+      const shape = new ShapeClass();
+      shape.setColor(shapeColor);
+      // creates a new SVG object, and then we set the text content and the text color, finally we set the shape, all based on what the user chose in the prompts
+      const svg = new SVG();
+      svg.setText(text, textColor);
+      svg.setShape(shape);
+});
 // after this, we will chain .then (what will we do with our response, or what potential issues we need to check for before writing)
 
 
@@ -69,4 +91,3 @@ function promptUser() {
 // tldr
 // We will use classes to define what a logo can be, constructors to create new logos with specific details, 
 // and objects to represent the actual logos we create. 
-// It helps us organize and manage our logos in a structured and organized way.
